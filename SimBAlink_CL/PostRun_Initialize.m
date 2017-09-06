b415f52b47e486e93c71063fefd2f836585e313e
ringler28 <ringler.28@osu.edu>
2017-09-06 19:15:11 -0400

    combined time and distance simulations into one

---
+++ SimBAlink_CL/PostRun_Initialize.m
@@ -0,0 +1,14 @@
+%Post processing script run after simulation end/termination
+
+%% Autoplotting Functions
+
+
+
+%% SOC Depleted Error 
+    %Message alert is displayed if simulation terminated due to no energy
+    if SOC_Batt(end) <0.1
+      msgbox('You are out of energy dumbass!');
+    end
+    
+%% Unknown Error
+    %put something here that says something when something happens
